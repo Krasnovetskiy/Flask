cae7cd06ae26be60c56d1a888d16b312649320c5
Vkrasnovetskyi <valeriykrasnoveckii@gmail.com>
2023-04-15 12:19:32 +0300

    добавил страницы регистрация и логин

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
-from flask import Flask
+from flask import Flask, request, render_template
+
 
 app = Flask(__name__)
 
+@app.route("/", methods=['GET', 'POST'])
+def index(*args, **kwargs):
+    context = {'name': 'Alex'}
+    if request.method == 'POST':
+        name = request.form.get('name')
+        password = request.form.get('password')
+        #logic
+        context.update({'name': name, 'password': password})
+    return render_template('index.html', **context)
 
-def index():
-    ...
+@app.route("/registration", methods=['GET', 'POST'])
+def registration():
+    return render_template('registration.html')
+
+@app.login("/login")
+def login():
+    return render_template('login.html')
+
